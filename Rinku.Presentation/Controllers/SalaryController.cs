85fa288a336077741a1da8958031dcfb20176e5f
MCepedaM <mh_cepeda@outlook.com>
2018-08-11 20:04:07 -0500

    [Front-End] Calculo de nomina

---
+++ Rinku.Presentation/Controllers/SalaryController.cs
@@ -0,0 +1,51 @@
+﻿using Rinku.Presentation.ExternalServices;
+using Rinku.Presentation.Helpers;
+using Rinku.Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Web.Mvc;
+
+
+namespace Rinku.Presentation.Controllers
+{
+    public class SalaryController : Controller
+    {
+        // GET: Salary
+        public ActionResult Index()
+        {
+            return View(new List<SalaryViewModel>());
+        }
+
+       [HttpGet]
+       public ActionResult GetCalculoNomina(string fechaString, int idEmpleado)
+       {
+            DateTime fecha = DateTime.ParseExact(fechaString, "d-M-yyyy", CultureInfo.InvariantCulture);
+            SalaryViewModel salModel = new SalaryViewModel();
+            StringBuilder sb = new StringBuilder();
+            var serviceNomina = GetService.nominaService;
+            var lstSuedos = serviceNomina.GetSalarios(fecha, fecha.AddMonths(1), idEmpleado);
+
+            foreach (var sueldo in lstSuedos)
+            {
+                salModel = new SalaryViewModel();
+                salModel.CopyPropertiesFrom(sueldo);
+                sb.Append("<tr><td>" + salModel.Nombre + "</td><td>" +
+                            salModel.getDescripcionRol() + "</td><td>" +
+                            (salModel.interno ? "Si" : "No") + "</td><td>" +
+                            salModel.DiasLaborados + "</td><td>" +
+                            salModel.Entregas + "</td><td>" +
+                            salModel.Adicional + "</td><td>" +
+                            salModel.BonoXHora + "</td><td>" +
+                            salModel.Subtotal + "</td><td>" +
+                            "( " + salModel.PorcentajeIva + "%) " + salModel.MontoIva + "</td><td>" +
+                            salModel.MontoTotal + "</td><td>" +
+                            salModel.ValesDespensa + "</td><td>" +
+                                (salModel.MontoTotal + salModel.ValesDespensa).ToString("#.##") + "</ td ></ tr > ");
+            }
+            return Content(sb.ToString());
+
+       }
+    }
+}
---
+++ Rinku.Presentation/Models/SalaryViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rinku.Presentation.Models
+{
+    public class SalaryViewModel
+    {
+        [Display(Name = "Identificador Único")]
+        public int Id { get; set; }
+        [Display(Name = "Nombre empleado")]
+        public string Nombre { get; set; }
+        [Display(Name = "Rol")]
+        public Int16 Rol { get; set; }
+        [Display(Name = "Interno")]
+        public bool interno { get; set; }
+        [Display(Name = "Dias laborados")]
+        public int DiasLaborados { get; set; }
+        [Display(Name = "Entregas")]
+        public int Entregas { get; set; }
+        [Display(Name = "Bono Adicional")]
+        public decimal Adicional { get; set; }
+        [Display(Name = "Bono x Hora")]
+        public decimal BonoXHora { get; set; }
+        [Display(Name = "Subtotal")]
+        public decimal Subtotal { get; set; }
+        [Display(Name = "% Iva")]
+        public decimal PorcentajeIva { get; set; }
+        [Display(Name = "Monto Iva")]
+        public decimal MontoIva { get; set; }
+        [Display(Name = "Monto")]
+        public decimal MontoTotal { get; set; }
+        [Display(Name = "Vales de despensa")]
+        public decimal ValesDespensa { get; set; }
+
+        public decimal totalAPagar()
+        {
+            return MontoIva + ValesDespensa;
+        }
+        public string getDescripcionRol()
+        {
+            string descripcion = "";
+            switch (Rol)
+            {
+                case 1:
+                    descripcion = "Chofer";
+                    break;
+                case 2:
+                    descripcion = "Cargador";
+                    break;
+                default:
+                    descripcion = "Auxiliar";
+                    break;
+            }
+
+            return descripcion;
+        }
+
+    }
+}
